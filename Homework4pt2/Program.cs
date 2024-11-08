5492501d7ea8ba6755b51b53d44a0f805979de94
Emily Sierra <sierraemely@gmail.com>
2024-11-08 14:58:37 -0800

    Add project files.

---
+++ Homework4pt2/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int totalTosses = 0;
+        String userInput;
+        int counter = 0;
+        int totalHeads = 0;
+        int totalTails = 0;
+        double percentTails;
+        double percentHeads;
+
+        Console.WriteLine("For each coin toss enter either 'h' for heads or 't' for tails");
+
+        do
+        {
+            Console.Write("Toss # " + totalTosses + " : ");
+            userInput = Console.ReadLine();
+            if (userInput == "h")
+            {
+                totalHeads++;
+                totalTosses++;
+            }
+            else if (userInput == "t")
+            {
+                totalTails++;
+                totalTosses++;
+            }
+            //percentHeads = (totalHeads / totalTosses) * 100.00;
+            //percentTails = (totalTails / totalTosses) * 100.00;
+
+        } while (totalTosses < 8);
+
+        percentHeads = (totalHeads / (double)totalTosses) * 100.00;
+        percentTails = (totalTails / (double)totalTosses) * 100.00;
+        Console.WriteLine("Number of heads: " + totalHeads);
+        Console.WriteLine("Number of tails: " + totalTails);
+        Console.WriteLine("Percent of heads: " + percentHeads);
+        Console.WriteLine("Percent of tails: " + percentTails);
+
+    }
+}
